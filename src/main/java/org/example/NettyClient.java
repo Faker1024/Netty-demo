2c50c7ff99119087558d345c6e863a0992c234c3
jack <3361935899@qq.com>
2024-06-22 12:24:07 +0800

    Netty Client

---
+++ src/main/java/org/example/NettyClient.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class NettyClient {
+
+    private static final int MAX_RETRY = 5;
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功");
+            } else if (retry == 0) {
+                System.out.println("已无重试次数， 连接失败");
+            } else {
+                int order = (MAX_RETRY - retry) + 1;
+                int delay = 1 << order;
+                System.out.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Bootstrap clientBootstrap = new Bootstrap();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        clientBootstrap.group(workGroup)
+                .channel(NioSocketChannel.class)
+                .attr(AttributeKey.newInstance("clientName"), "nettyClient")
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                    }
+                });
+        connect(clientBootstrap, "127.0.0.1", 8000, MAX_RETRY);
+    }
+}
